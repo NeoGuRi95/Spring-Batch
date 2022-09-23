bce95256202a5e3792760cd4e018cc82f1526be0
NeoGuRi95 <jea5158@gmail.com>
2022-09-23 13:33:42 +0900

    job instance id(혹은 paramenter)가 같을 경우 이전에 실패한 스텝만 실행

--- src/main/java/com/ll/exam/app_2022_09_22/job/helloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/ll/exam/app_2022_09_22/job/helloWorld/HelloWorldJobConfig.java
@@ -1,11 +1,14 @@
 package com.ll.exam.app_2022_09_22.job.helloWorld;
 
+import jdk.jfr.StackTrace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -24,30 +27,47 @@ public class HelloWorldJobConfig {
     @Bean
     public Job helloWorldJob() {
         return jobBuilderFactory.get("helloWorldJob")
-                .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행시에 파라미터로 부여
+                //.incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행시에 파라미터로 부여
                 .start(helloWorldStep1())
                 .next(helloWorldStep2())
                 .build();
     }
 
     @Bean
+    @JobScope
     public Step helloWorldStep1() {
         return stepBuilderFactory.get("helloWorldStep1")
-                .tasklet(helloWorldTasklet())
+                .tasklet(helloWorldTasklet1())
                 .build();
     }
 
     @Bean
+    @JobScope
     public Step helloWorldStep2() {
         return stepBuilderFactory.get("helloWorldStep2")
-                .tasklet(helloWorldTasklet())
+                .tasklet(helloWorldTasklet2())
                 .build();
     }
 
     @Bean
-    public Tasklet helloWorldTasklet() {
+    @StepScope
+    public Tasklet helloWorldTasklet1() {
         return (contribution, chunkContext) -> {
-            System.out.println("헬로월드!");
+            System.out.println("헬로월드_1");
+
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet helloWorldTasklet2() {
+        return (contribution, chunkContext) -> {
+            System.out.println("헬로월드_2");
+
+            if (false) {
+                throw new Exception("실패: 태스크릿 2");
+            }
 
             return RepeatStatus.FINISHED;
         };
